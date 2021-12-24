7716149d2b9e81a21c5d57dee8808c754eeff48b
ridhaadev <ridhaadev@gmail.com>
2021-12-24 21:35:30 +0200

    Added notes for lexical scoping

--- exercise_1point1.rkt
+++ exercise_1point1.rkt
@@ -2,7 +2,8 @@
 ; Exercises
 ;===============================================
 
-;1.1)
+; ====
+; Exercise 1.1)
 ;Evaluate (I used the substition model here)
 ;10 -> 10
 ;(+ 5 3 4) -> 12
@@ -29,7 +30,8 @@
 ;         (else -1))
 ;   (+ a 1)) -> 16
 
-; 1.2)
+; ====
+; Exercise 1.2)
 ;Translate the image into prefix form:
 ;(My interpretation of the image)
 ;5 + a half + (2 - (3 - (6 + a third))
@@ -46,8 +48,11 @@
 ;      (- 6 2)
 ;      (- 2 7)))
 
-;Exercise 1.3
-; Sum of squares already defined above
+; ====
+; Exercise 1.3
+(define (sum-of-squares x y)
+  (+ (* x x) (* y y)))
+
 (define (sum-of-largest-squares a b c)
   (cond ((and (and (>= a b) (>= a c))
              (and (>= b c)))
@@ -61,15 +66,17 @@
         )
   )
 
-(sum-of-largest-squares 4 2 3)
+;(sum-of-largest-squares 4 2 3)
 
-;Exercise 1.4
+; ====
+; Exercise 1.4
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 ;This procedure appears to condtionally add or subtract a from b based on
 ;if b is positive or negative. Meaning it will always add the postive
 ;version of b
 
+; ====
 ; Exercise 1.5
 (define (p) (p))
 
@@ -104,3 +111,42 @@
 ;
 ;The expression evalutes to true, and 0 will be returned, kind of short-circuiting
 ;and never evaluating (p)
+
+; ====
+; Exercise 1.6
+
+; Alyssa's function will be stuck in the recursive call of sqrt-iter
+; because since the custom new-if procedure isn't a special form, the interpreter
+; will evaluate the recursive case before it knows what the base case is.
+
+; ====
+; Exercise 1.7
+
+; For getting the square root of extreme values, the given tolerance might not quite
+; be sufficient and have a really large error, requiring many, many iterations.
+; Because you don't know this beforehand, you might supply too few iterations for
+; smaller values or too low a precision for larger values.
+
+; An alternative way which solves this problem would be to observe the difference
+; between the previous guess and the next. Since Newtons algorithm converges, there
+; will inevitably come a threshold where the difference between iterations are
+; negligable. The code:
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x) 
+  (= (improve guess x) guess)) ; Check to see if the next guess = the previous guess
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
